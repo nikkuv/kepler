f9c831c88644361ace177ec12a3ce95bc188beed
nikkuv <2000vermasaumya18@gmail.com>
2024-08-09 01:28:48 +0530

    add kepler

---
+++ src/script.js
@@ -0,0 +1,107 @@
+
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import GUI from 'lil-gui'
+
+/**
+ * Base
+ */
+// Debug
+// const gui = new GUI()
+
+// Canvas
+const canvas = document.querySelector('canvas.webgl')
+
+// Scene
+const scene = new THREE.Scene()
+
+/**
+ * Textures
+ */
+const textureLoader = new THREE.TextureLoader()
+const sphereTexture = textureLoader.load('textures/kepler.png')
+
+sphereTexture.wrapS = THREE.RepeatWrapping;
+sphereTexture.wrapT = THREE.RepeatWrapping;
+
+// Material
+const material = new THREE.MeshStandardMaterial({
+    map: sphereTexture,
+    roughness: 1, 
+    metalness: 0.1,
+})
+
+// Mesh
+const geometry = new THREE.SphereGeometry(1, 64, 64);
+const sphere = new THREE.Mesh(geometry, material)
+scene.add(sphere)
+
+// Lighting
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.5);
+scene.add(ambientLight);
+
+const pointLight = new THREE.PointLight(0xffffff, 1);
+pointLight.position.set(5, 5, 5);
+scene.add(pointLight);
+
+/**
+ * Sizes
+ */
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    // Update sizes
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    // Update camera
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    // Update renderer
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+/**
+ * Camera
+ */
+// Base camera
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.x = 1
+camera.position.y = 1
+camera.position.z = 2
+scene.add(camera)
+
+// Controls
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+/**
+ * Renderer
+ */
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+/**
+ * Animate
+ */
+function animate() {
+    requestAnimationFrame(animate);
+
+    // Rotate the sphere
+    sphere.rotation.y += 0.003;
+
+    // Render the scene
+    renderer.render(scene, camera);
+}
+
+// Start the animation loop
+animate();
